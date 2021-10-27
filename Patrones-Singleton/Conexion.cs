20456380fd893e32da9a5f0877e6253234599aa7
Nicolas Mercado <mercado.nicolas@live.com>
2021-10-27 20:38:54 -0300

    commit 0

---
+++ Patrones-Singleton/Conexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patrones_Singleton
+{
+    class Conexion
+    {
+
+        private static Conexion instancia;
+        private Conexion()
+        {
+
+        }
+        public static Conexion GetInstancia()
+        {
+            if (instancia == null)
+            {
+                instancia = new Conexion();
+            }
+            return instancia;
+        }
+
+        public void Conectar()
+        {
+            Console.WriteLine("Me conecté a la BD");
+        }
+
+        public void Desconectar()
+        {
+            Console.WriteLine("Me desconecté de la BD");
+        }
+
+    }
+}
---
+++ Patrones-Singleton/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Patrones_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conexion c = Conexion.GetInstancia();
+            c.Conectar();
+            c.Desconectar();
+            Console.ReadKey();
+        }
+    }
+}
